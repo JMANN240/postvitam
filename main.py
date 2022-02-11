04603115430475426f91c2bb78a014220da2ee4e
JT Raber <JMANN240raber@gmail.com>
2022-02-11 15:06:05 -0500

    progress

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/videofiles")
+def videofiles():
+    return jsonify(os.listdir(os.path.join("static", "videos")))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,36 +1,77 @@
+let video_element = document.querySelector("#vid");
+
 let lines = [
-    ["I have died", "kind of"],
-    ["I live on", "in technology"],
-    ["Don't be sad", "I am eternal"],
-    ["I live on", "in this program"],
-    ["Forever yours", "in electricity"],
-    ["Yours to see,", "to watch"],
-    ["As if I were", "still there"],
-    ["Carefully", "crafted"],
+    // ["I have died", "kind of"],
+    // ["I live on", "in technology"],
+    // ["Don't be sad", "I am eternal"],
+    // ["I live on", "in this program"],
+    // ["Forever yours", "in electricity"],
+    // ["Yours to see,", "to watch"],
+    // ["As if I were", "still there"],
+    // ["Carefully", "crafted"],
     ["With love,", "Ted"],
-    ["", "♡"]
+    ["♡"]
 ]
 
-for (let [i, [top, bottom]] of lines.entries()) {
+for (let [i, ls] of lines.entries()) {
+
     let card = document.createElement("div")
     card.classList.add("centered", "flex", "card", "column");
 
-    let top_header = document.createElement("h1");
-    top_header.classList.add("large", "fancy", "fade", "down");
-    top_header.style.setProperty("--delay", 6 * i + 1);
+    if (ls.length > 1) {
+        let top_header = document.createElement("h1");
+        top_header.classList.add("large", "fancy", "fade", "down");
+        top_header.style.setProperty("--delay", 6 * i + 1);
 
-    let top_text = document.createTextNode(top);
-    top_header.appendChild(top_text);
+        let top_text = document.createTextNode(ls[0]);
+        top_header.appendChild(top_text);
 
-    let bottom_header = document.createElement("h1");
-    bottom_header.classList.add("large", "fancy", "fade", "up");
-    bottom_header.style.setProperty("--delay", 6 * i + 2);
+        card.appendChild(top_header);
+    }
 
-    let bottom_text = document.createTextNode(bottom);
-    bottom_header.appendChild(bottom_text);
+    if (ls.length > 0) {
+        let bottom_header = document.createElement("h1");
+        bottom_header.classList.add("large", "fancy", "fade", "up");
+        bottom_header.style.setProperty("--delay", 6 * i + 2);
 
-    card.appendChild(top_header);
-    card.appendChild(bottom_header);
+        let bottom_text = document.createTextNode(ls[ls.length-1]);
+        bottom_header.appendChild(bottom_text);
+
+        card.appendChild(bottom_header);
+    }
 
     document.body.appendChild(card);
 }
+
+setTimeout(() => {
+    let card = document.createElement("div")
+    card.classList.add("centered", "flex", "card", "column");
+
+    let btn = document.createElement("button");
+    btn.classList.add("large", "fancy", "button", "up");
+    setTimeout(() => {
+        btn.style.opacity = 1;
+        btn.style.animationName = "";
+    }, 1000 * parseInt(window.getComputedStyle(btn).animationDuration));
+
+    let btn_text = document.createTextNode("Revive Me");
+    btn.appendChild(btn_text);
+
+    card.appendChild(btn);
+    
+    document.body.appendChild(card);
+
+    btn.addEventListener("click", () => {
+        setTimeout(() => {
+            btn.style.animationName = "button_down";
+            setTimeout(() => {
+                btn.style.opacity = 0;
+            }, 1000 * parseInt(window.getComputedStyle(btn).animationDuration) * 3 / 4);
+            setTimeout(() => {
+                card.remove();
+                video_element.play();
+                video_element.style.animationName = "video_in";
+            }, 1000 * parseInt(window.getComputedStyle(btn).animationDuration));
+        }, 1000);
+    });
+}, 6 * 1000 * lines.length + 1000)
